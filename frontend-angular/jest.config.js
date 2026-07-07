31d540ddc5e69fbf32438fc8fd84c272a1adeff3
William Medina <williamir1234@gmail.com>
2026-07-07 16:51:21 -0500

    test(frontend): configurar Jest y agregar prueba base

---
+++ frontend-angular/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@core/(.*)$': '<rootDir>/src/app/core/$1',
+    '^@shared/(.*)$': '<rootDir>/src/app/shared/$1',
+    '^@features/(.*)$': '<rootDir>/src/app/features/$1',
+    '^@env/(.*)$': '<rootDir>/src/environments/$1'
+  }
+};
---
+++ frontend-angular/src/app/core/baseline.spec.ts
@@ -0,0 +1,7 @@
+import { TestBed } from '@angular/core/testing';
+
+describe('Jest Testing Baseline', () => {
+  it('should run tests successfully', () => {
+    expect(true).toBe(true);
+  });
+});
