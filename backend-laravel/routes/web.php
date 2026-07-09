f8ffe1c6bb865af37edfb1753df6253c36ddf69d
William Medina <williamir1234@gmail.com>
2026-07-08 22:27:10 -0500

    Añadir ruta temporal de migracion

--- backend-laravel/routes/web.php
+++ backend-laravel/routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::view('/', 'welcome');
+
+Route::get('/run-migrations', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migraciones ejecutadas exitosamente. ' . Artisan::output();
+});
