abb470ff04a17cb492396ea05b68d1e0bcf90b8c
William Medina <williamir1234@gmail.com>
2026-07-23 00:21:34 -0500

    feat(aula): configurar Firestore y layout base

--- frontend-angular/src/app/app.config.ts
+++ frontend-angular/src/app/app.config.ts
@@ -1,7 +1,15 @@
 import { registerLocaleData } from '@angular/common';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import es from '@angular/common/locales/es';
-import { ApplicationConfig, LOCALE_ID, isDevMode, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  LOCALE_ID,
+  inject,
+  isDevMode,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter, withInMemoryScrolling, withPreloading } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
 import { es_ES, provideNzI18n } from 'ng-zorro-antd/i18n';
@@ -9,6 +17,7 @@ import { provideSpinnerConfig } from 'ngx-spinner';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { routes } from './app.routes';
 import { tokenInterceptor } from './core/interceptores/token-interceptor';
+import { FirestoreApp } from './core/servicios/firestore-app';
 import { SelectivePreloadingStrategy } from './core/servicios/selective-preloading.strategy';
 
 registerLocaleData(es);
@@ -27,6 +36,9 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors([tokenInterceptor])),
     provideNzI18n(es_ES),
     provideSpinnerConfig({ type: 'square-jelly-box' }),
+    provideAppInitializer(() => {
+      return inject(FirestoreApp).inicializar();
+    }),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerImmediately',
